62fca951237f90c5a08c69ee6d5ff38d28f398c5
Maarten de Jonge <mdejonge1990@gmail.com>
2012-12-21 12:19:57 +0100

    add example code to show how proxies and stuff work

---
+++ NaoLeague/MotionSkeleton/main.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the COPYING file.
+ */
+#include <iostream>
+#include <string>
+
+// include the proxies you'll be using
+#include <alproxies/almotionproxy.h>
+
+#define PORT 9559
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // quick and dirty command parsing to get the IP
+    string ip = string( argv[1] );
+
+    // create a proxy, specifying the IP and port (port is always 9559)
+    AL::ALMotionProxy motProxy(ip, PORT);
+
+    // make sure the entire robot is stiff
+    motProxy.setStiffnesses("Body", 0.8);
+
+    while (true) {
+        motProxy.setAngles("RShoulderRoll", 0, 0.7);
+        motProxy.setAngles("RShoulderRoll", 1, 0.7);
+    }
+
+    return 0;
+}
