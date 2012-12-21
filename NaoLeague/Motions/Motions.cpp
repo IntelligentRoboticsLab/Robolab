4948c22a7bb6f4b4beb91b007064143158b0bdf3
Maarten de Jonge <mdejonge1990@gmail.com>
2012-12-21 20:47:11 +0100

    motion interpolation with JSON data implemented

--- NaoLeague/Motions/Motions.cpp
+++ NaoLeague/Motions/Motions.cpp
@@ -7,10 +7,19 @@ using namespace std;
 using namespace AL;
 using boost::property_tree::ptree;
 
+typedef pair<string, string> str_str_pair;
+
 Motions::Motions(string naoqi_ip)
-    //: m_motProxy(naoqi_ip)
+    : m_motProxy(naoqi_ip)
 {
-    parseMotionFile( string("test.js"), string("testing"));
+    m_motion_filenames.push_back(pair<string, string>("Motions/getupFromBack1.json", "getupFromBack1") );
+
+    // parse each motion file
+    BOOST_FOREACH (str_str_pair &file_key_pair, m_motion_filenames) {
+        parseMotionFile(file_key_pair.first, file_key_pair.second);
+    }
+
+    cout << m_interpolations.size() << endl;
 }
 
 Motions::~Motions()
@@ -37,15 +46,15 @@ void Motions::parseMotionFile(string filename, string key)
     // iterate through each joint contained in the root list, and append its
     // values to the InterpolationValues struct
     BOOST_FOREACH (const ptree::value_type &element, pt.get_child("root")) {
-        vector<double> angles_vector;
-        vector<double> times_vector;
+        vector<float> angles_vector;
+        vector<float> times_vector;
 
         // read the angle and time values as a vector
         BOOST_FOREACH (const ptree::value_type& ele, element.second.get_child("angles")) {
-            angles_vector.push_back(ele.second.get<double>(""));
+            angles_vector.push_back(ele.second.get<float>(""));
         }
         BOOST_FOREACH (const ptree::value_type& ele, element.second.get_child("times")) {
-            times_vector.push_back(ele.second.get<double>(""));
+            times_vector.push_back(ele.second.get<float>(""));
         }
 
         ip_values.names.push_back( element.second.get<string>("name") );
@@ -54,6 +63,28 @@ void Motions::parseMotionFile(string filename, string key)
     }
 
     m_interpolations[key] = ip_values;
+}
+
+void Motions::interpolate(string key)
+{
+    InterpolationValues iv = m_interpolations[key];
+    
+    // convert the vectors into ALValues
+    // TODO: just make them ALValues in the first place
+    ALValue al_angles,
+            al_times;
+
+    unsigned size = iv.angles.size();
+    al_angles.arraySetSize(size);
+    al_times.arraySetSize(size);
+
+    for (unsigned i = 0; i < size; ++i) {
+        al_angles[i] = ALValue::array(iv.angles[i]);
+        al_times[i] = ALValue::array(iv.angles[i]);
+    }
+
+    // actuate!
+    m_motProxy.angleInterpolation(iv.names, al_angles, al_times, true);
 }
 
 int main(int argc, char const *argv[])
--- NaoLeague/Motions/Motions.h
+++ NaoLeague/Motions/Motions.h
@@ -34,16 +34,21 @@ class Motions
          */
         struct InterpolationValues {
             vector<string> names;
-            vector<vector<double> > angles;
-            vector<vector<double> > times;
+            vector<vector<float> > angles;
+            vector<vector<float> > times;
         };
 
         /**
          * Holds interpolationvalues to be used by motions.
          */
         map<string, InterpolationValues> m_interpolations;
+
+        /**
+         * Holds the filenames and desired keys of the json files containg moves
+         */
+        vector<pair<string, string> > m_motion_filenames;
         
-        //AL::ALMotionProxy m_motProxy;
+        AL::ALMotionProxy m_motProxy;
 
         /**
          * Parses a JSON file containg a motion interpolation, and inserts it
@@ -53,6 +58,15 @@ class Motions
          * @param key The key to use for insertion into m_interpolations.
          */
         void parseMotionFile(string filename, string key);
+
+        /**
+         * Interpolates a motion using the values retrieved from
+         * m_interpolations.
+         *
+         * @param key The key belonging to the interpolation values in
+         * m_interpolations.
+         */
+        void interpolate(string key);
 };
 
 #endif
