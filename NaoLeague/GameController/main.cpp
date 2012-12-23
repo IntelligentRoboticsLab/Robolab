47ca6a914cee6f8a5b3a8c91bef0a1d2ca68c388
Steven <steven@ubuntu.(none)>
2012-12-23 22:57:02 +0100

    GameController can read all information

--- NaoLeague/GameController/main.cpp
+++ NaoLeague/GameController/main.cpp
@@ -1,7 +1,7 @@
-/*
- * Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the COPYING file.
+/* 
+ * Program that can read data from the Robocup GameController
+ *
+ * @author Steven Laan
  */
 
 #include <iostream>
@@ -17,7 +17,7 @@
 
 #define BUF_SIZE 512
 
-void slice( char data[], char sliced[], int begin, int end );
+void fillGCData( char buf[], struct RoboCupGameControlData * GCData );
 
 int main( int argc, char *argv[] ) {
   struct addrinfo hints;
@@ -25,7 +25,7 @@ int main( int argc, char *argv[] ) {
 
   struct RoboCupGameControlData GCData;
 
-  int sfd, s, j;
+  int sfd, s;
   size_t len;
   ssize_t nread;
   char buf[BUF_SIZE];
@@ -60,7 +60,7 @@ int main( int argc, char *argv[] ) {
       continue;
 
     // Try to bind
-    if ( bind(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
+    if ( bind( sfd, rp->ai_addr, rp->ai_addrlen ) != -1)
       break;
 
     // Close this socket if we could not connect
@@ -69,7 +69,7 @@ int main( int argc, char *argv[] ) {
 
   // Check if we're connected
   if ( rp == NULL ) {
-     fprintf( stderr, "Could not connect\n");
+     fprintf( stderr, "Could not connect\n" );
      exit( EXIT_FAILURE );
   }
   
@@ -82,43 +82,73 @@ int main( int argc, char *argv[] ) {
     if ( nread == -1 ) {
       perror( "Error while reading" );
       exit( EXIT_FAILURE );
-    } 
-
-    // Warning: order of bytes!
-    GCData.version        = ( buf[4] << 0  ) | 
-                            ( buf[5] << 8  ) | 
-                            ( buf[6] << 16 ) | 
-                            ( buf[7] << 32 );
-    GCData.playersPerTeam = buf[8];
-    GCData.state          = buf[9];
-    GCData.firstHalf      = buf[10];
-    GCData.kickOffTeam    = buf[11];
-    GCData.secondaryState = buf[12];
-    GCData.dropInTeam     = buf[13];
-    GCData.dropInTime     = ( buf[14] << 0 ) | 
-                            ( buf[15] << 0 );
-    GCData.secsRemaining  = ( buf[16] << 0 ) | 
-                            ( buf[17] << 8 ) | 
-                            ( buf[18] << 16  ) | 
-                            ( buf[19] << 32  );
-
+    }
+    
+    // TODO: Check header
+    fillGCData( buf, &GCData );
+    
     // Print statements for debugging purposes
-    printf( "Version          = %d\n", GCData.version );
-    printf( "Players per team = %d\n", GCData.playersPerTeam );
     printf( "State            = %d\n", GCData.state );
     printf( "firstHalf        = %d\n", GCData.firstHalf );
-    printf( "KickOffTeam      = %d\n", GCData.kickOffTeam );
     printf( "secondaryState   = %d\n", GCData.secondaryState );
     printf( "dropInTeam       = %d\n", GCData.dropInTeam );
     printf( "dropInTime       = %d\n", GCData.dropInTime );
     printf( "secsRemaining    = %d\n", GCData.secsRemaining );
-
-    // TODO: Read team info
-
-    printf( "Received %ld bytes: %s\n", (long) nread, buf );  
+    printf( "%d - %d\n", GCData.teams[0].score, GCData.teams[1].score );
   }
 
   close( sfd );
-  exit(EXIT_SUCCESS);
+  exit( EXIT_SUCCESS );
 }
 
+/*
+ * Fills the given structure with the values from the buffer. Assumes that
+ * values are little-endian.
+ *
+ * Note: Explicit use of non postfix increments, as they can have weird side-
+ * effects when used in combined expressions.
+ */
+void fillGCData( char buf[], struct RoboCupGameControlData * GCData ) {
+  // Warning: order of bytes!
+  // Note that all characters are implicitly converted to ints
+  int i = 4;
+  
+  // Read global data
+  GCData->version        = ( buf[i]   << 0  ) | 
+                           ( buf[i+1] << 8  ) | 
+                           ( buf[i+2] << 16 ) | 
+                           ( buf[i+3] << 32 );
+  i += 4;
+  GCData->playersPerTeam = buf[i++];
+  GCData->state          = buf[i++];
+  GCData->firstHalf      = buf[i++];
+  GCData->kickOffTeam    = buf[i++];
+  GCData->secondaryState = buf[i++];
+  GCData->dropInTeam     = buf[i++];
+  GCData->dropInTime     = ( buf[i]   << 0  ) | 
+                           ( buf[i+1] << 8  );
+  i += 2;
+  GCData->secsRemaining  = ( buf[i]   << 0  ) | 
+                           ( buf[i+1] << 8  ) | 
+                           ( buf[i+2] << 16 ) | 
+                           ( buf[i+3] << 32 );
+  i += 4;
+
+  // Read data per team
+  for ( int team = 0; team < 2; team++ ) {
+    GCData->teams[team].teamNumber = buf[i++];
+    GCData->teams[team].teamColour = buf[i++];
+    GCData->teams[team].goalColour = buf[i++];
+    GCData->teams[team].score      = buf[i++];
+    
+    // Read data per player/robot
+    for ( int bot = 0; bot < MAX_NUM_PLAYERS; bot++ ) {
+      GCData->teams[team].players[bot].penalty             = ( buf[i]   << 0 ) |
+                                                             ( buf[i+1] << 8 );
+      i += 2;
+      GCData->teams[team].players[bot].secsTillUnpenalised = ( buf[i]   << 0 ) |
+                                                             ( buf[i+1] << 8 );
+      i += 2;
+    }
+  }
+}
