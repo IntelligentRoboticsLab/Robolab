05da6b8bba32d2a8a880a6ba9940ae783524aa55
Patrick de Kok <patrick@cxiu.nl>
2013-01-10 18:01:23 +0100

    Emptied erroneous soul

--- NaoLeague/soul.cpp
+++ NaoLeague/soul.cpp
@@ -4,21 +4,10 @@
  * found in the COPYING file.
  */
 #include <iostream>
-#include <opencv2\highgui\highgui.hpp>
-#include <opencv2\core\core.hpp>
-#include "Vision\imageGrabber.h"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include "Vision/imageGrabber.h"
 
 int main()
 {
-	ImageGrabber imageGrabber("169.254.95.24");
-	ImageContainer* imageContainer;
-	imageContainer = NULL;
-	while (true){
-		if(imageContainer != NULL){
-			delete imageContainer;
-		}
-		imageContainer = imageGrabber.pollImage();
-		cv::imshow("Polled Image", imageContainer->image);
-		cv::waitKey(10);
-	}
 }
