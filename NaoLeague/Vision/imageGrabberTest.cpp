4b475ccdbc01fca5fada97bcb04810841a9fd02e
Patrick de Kok <patrick@cxiu.nl>
2013-01-11 13:44:48 +0100

    Wrong slashes for Linux

--- NaoLeague/Vision/imageGrabberTest.cpp
+++ NaoLeague/Vision/imageGrabberTest.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <opencv2\highgui\highgui.hpp>
-#include <opencv2\core\core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
 #include "imageGrabber.h"
 
 int main()
